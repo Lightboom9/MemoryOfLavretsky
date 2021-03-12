1a2db30a41220ca8e83bea595ad0f8118664dd8f
Lightboom <55315954+Lightboom9@users.noreply.github.com>
2021-03-12 21:42:58 +0300

    tidying things up, moving them into a class

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import os
 from pydub import AudioSegment
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-#data-mp3
-
 
 def captcha_handler(captcha):
     key = input("Enter captcha code {0}: ".format(captcha.get_url())).strip()
@@ -16,57 +14,55 @@ def captcha_handler(captcha):
     return captcha.try_again(key)
 
 
-def send_voice_message(vk, vk_session, chat_id, audio_filename):
-    audio_message_url = vk_session.method('docs.getUploadServer', {'type': 'audio_message'})['upload_url']
-    files = [('file', (audio_filename, open(audio_filename, 'rb')))]
-    uploaded_url = requests.post(audio_message_url, files=files).text
-    response = json.loads(uploaded_url)['file']
-    uploaded_response = vk_session.method('docs.save', {'file': response})
-    file_id = uploaded_response['audio_message']['id']
-    owner_id = uploaded_response['audio_message']['owner_id']
+class VkController:
+    def __init__(self, chat_ids):
+        with open('pass.txt', encoding='utf-8') as f:
+            passes = f.readlines()
 
-    document = 'doc%s_%s' % (str(owner_id), str(file_id))
-    vk.messages.send(chat_id=chat_id, random_id=random.randint(0, 999999999999999), message='', attachment=document)
+        self.vk_session = vk_api.VkApi(passes[0], passes[1], app_id=2685278, captcha_handler=captcha_handler, scope="audio,docs,messages")
+        self.vk_session.auth()
+        self.vk = self.vk_session.get_api()
 
+        with open('quotes.txt', encoding='utf8') as f:
+            quotes = f.readlines()
+        self.quotes = [s[0:s.find('©')].rstrip() for s in quotes]
 
-def send_random_voice_message(vk, vk_session, chat_id):
-    audio = AudioSegment.from_file('NaziVoices' + str(random.randint(1, 16)) + '.ogg', format='ogg')
+        self.chat_ids = chat_ids
 
-    audio_length = len(audio)
-    part_length = random.randint(20000, 40000)
-    part_start = random.randint(0, audio_length - part_length)
-    part_end = part_start + part_length
+    def send_random_quotes(self):
+        for chat_id in self.chat_ids:
+            self.vk.messages.send(chat_id=chat_id, message=self.quotes[random.randint(0, len(self.quotes) - 1)], random_id=random.randint(0, 999999999999999))
+            time.sleep(random.randint(5, 10))
 
-    export_filename = 'NaziVoicesToUpload.ogg'
-    audio_final = audio[part_start:part_end]
-    audio_final.export(export_filename, format='ogg')
+    def send_random_voicelines(self):
+        audio_message_url = self.vk_session.method('docs.getUploadServer', {'type': 'audio_message'})['upload_url']
+        export_filename = 'NaziVoicesToUpload.ogg'
 
-    send_voice_message(vk, vk_session, chat_id, export_filename)
-    os.remove(export_filename)
+        for chat_id in self.chat_ids:
+            audio = AudioSegment.from_file('NaziVoices' + str(random.randint(1, 16)) + '.ogg', format='ogg')
+            audio_length = len(audio)
+            part_length = random.randint(20000, 40000)
+            part_start = random.randint(0, audio_length - part_length)
+            part_end = part_start + part_length
+
+            audio_final = audio[part_start:part_end]
+            audio_final.export(export_filename, format='ogg')
+
+            files = [('file', (export_filename, open(export_filename, 'rb')))]
+            uploaded_url = requests.post(audio_message_url, files=files).text
+            response = json.loads(uploaded_url)['file']
+            uploaded_response = self.vk_session.method('docs.save', {'file': response})
+            file_id = uploaded_response['audio_message']['id']
+            owner_id = uploaded_response['audio_message']['owner_id']
+
+            document = 'doc%s_%s' % (str(owner_id), str(file_id))
+            self.vk.messages.send(chat_id=chat_id, random_id=random.randint(0, 999999999999999), message='', attachment=document)
+            time.sleep(random.randint(5, 10))
 
 
 def main():
-    with open('quotes.txt', encoding='utf8') as f:
-        quotes = f.readlines()
-    quotes = [s[0:s.find('©')].rstrip() for s in quotes]
-
-    with open('pass.txt', encoding='utf-8') as f:
-        passes = f.readlines()
-
-    vk_session = vk_api.VkApi(passes[0], passes[1], app_id=2685278, captcha_handler=captcha_handler, scope="audio,docs,messages")
-    vk_session.auth()
-    vk = vk_session.get_api()
-
-    try:
-        vk_session.auth(token_only=True)
-    except vk_api.AuthError as error_msg:
-        print(error_msg)
-        return
-
-    chatid = 4
-    chatid2 = 5
-
-    send_random_voice_message(vk, vk_session, chatid)
+    vk = VkController([5])
+    vk.send_random_voicelines()
 
 '''
     longpoll = VkLongPoll(vk_session)
@@ -77,15 +73,8 @@ def main():
                 if event.text == '!Voice':
                     print('Sending voice message...')
                     send_voice_message(vk, vk_session, event.user_id, 'TestVoiceMsg.mp3')
-                    '''
+'''
 
-'''
-    while True:
-        vk.messages.send(chat_id=chatid, message=quotes[random.randint(0, len(quotes) - 1)], random_id=random.randint(0, 999999999999999))
-        time.sleep(random.randint(5, 15))
-        vk.messages.send(chat_id=chatid2, message=quotes[random.randint(0, len(quotes) - 1)], random_id=random.randint(0, 999999999999999))
-        time.sleep(random.randint(3600, 7200))
-'''
 
 if __name__ == '__main__':
     main()
